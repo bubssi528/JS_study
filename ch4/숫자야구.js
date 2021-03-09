97d7185086efcf39a0016fa33da98b81c59f8a0e
bubssi528 <intel1567@gmail.com>
2021-03-09 19:27:21 +0900

    Upgrade 숫자야구"js

--- ch4/숫자야구.js
+++ ch4/숫자야구.js
@@ -21,10 +21,11 @@ var 버튼 = document.createElement('button')
 버튼.textContent = '버튼';
 폼.append(버튼);
 
+var 실패 = 0;
 폼.addEventListener('submit',function 비동기(이벤트){
     이벤트.preventDefault();
+    //10번넘으면 끝
     // var 숫자배열 = String(숫자배열[0]) + String(숫자배열[1]) + String(숫자배열[2]) + String(숫자배열[3]); 
-    console.log(입력창.value, 숫자배열.join(''), 입력창.value === 숫자배열.join(''))
     if(숫자배열.join('') === 입력창.value){ 
         결과창.textContent = '홈런';
         입력창.value = '';
@@ -35,21 +36,35 @@ var 버튼 = document.createElement('button')
             var 뽑은것 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)),1)[0];
             숫자배열.push(뽑은것);
         }
+        실패 = 0;
     }
     else{
+        실패 += 1;
+        if(실패 > 10 ){
+            결과창.textContent = '10번넘게 틀려서 실패'
+            입력창.value = '';
+            입력창.focus();
+            숫자후보 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            숫자배열 = [];
+            for(var i = 0; i < 4; i +=1){
+                var 뽑은것 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)),1)[0];
+                숫자배열.push(뽑은것);
+            }
+            실패 = 0;
+        }
         var 스트라이크 = 0;
         var 볼 = 0;
         for(var i = 0; i < 4; i += 1){
-            console.log(숫자배열[i] , 입력창.value[i])
             if(String(숫자배열[i]) === 입력창.value[i]){
-                console.log("i : " + i,입력창[i])
                 스트라이크 += 1;
             } 
-            else if(String(숫자배열[i]).indexOf(입력창.value[i])){
+            else if(String(숫자배열).indexOf(입력창.value[i]) > -1){
+                console.log(i,String(숫자배열),입력창.value[i],String(숫자배열).indexOf(입력창.value[i]));
                 볼+=1;
             }
         }
-        결과창.textContent = '스트라이크' + 스트라이크 +'볼'+볼;
+        결과창.textContent = '스트라이크' + 스트라이크 +'볼'+볼 +'  실패:' + 실패;
+        입력창.focus();
     }
     }   
 )
